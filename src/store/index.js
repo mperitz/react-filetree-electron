4cbfcca7e7c77a0d5a36aa9c14041be4aa406475
Mike Peritz <mike.peritz@gmail.com>
2017-06-01 14:21:10 -0400

    removed dll folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import reducer from '../reducers';
-import logger from 'redux-logger'
 
 export default createStore(
-  reducer,
-  // applyMiddleware(logger)
+  reducer
 );
