f49e59a0859429853d96f891715719a3c8e556df
Mike Peritz <mike.peritz@gmail.com>
2017-06-01 10:30:49 -0400

    changed provider and store to be contained in component

--- src/containers/FileTreeContainer.js
+++ src/containers/FileTreeContainer.js
@@ -1,10 +1,22 @@
-import { connect } from 'react-redux';
+import { connect, Provider } from 'react-redux';
+import React, { Component } from 'react';
 
 import FileTree from '../components/FileTree';
 import { toggleVisibility } from '../reducers/file-tree';
+import store from '../store';
 
 const mapState = state => ({ isVisible: state.fileTree.isVisible });
 
 const mapDispatch = dispatch => ({ toggleVisibility: filePath => dispatch(toggleVisibility(filePath)) });
 
-export default connect(mapState, mapDispatch)(FileTree);
+const ConnectFileTree = connect(mapState, mapDispatch)(FileTree);
+
+export default class extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <ConnectFileTree {...this.props} />
+      </Provider>
+    )
+  }
+}
